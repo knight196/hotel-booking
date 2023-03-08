4ed79a7068a79cdb462a3755a5f81ea8b757419f
knight196 <71406824+knight196@users.noreply.github.com>
2023-03-08 12:47:41 +0000

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -92,7 +92,7 @@ app.post('/api/sendemail', async(req,res) => {
   try{
 
     const browser = await puppeteer.launch({
-      executablePath:process.env.executablePath
+      executablePath:'/opt/render/.cache/puppeteer'
     })
 
  const page = await browser.newPage();
