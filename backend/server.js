f0b2dd0551d5b500523aae228e69f38bc9470e67
knight196 <71406824+knight196@users.noreply.github.com>
2023-03-10 12:39:02 +0000

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -92,8 +92,7 @@ app.post('/api/sendemail', async(req,res) => {
   try{
 
     const browser = await puppeteer.launch({
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
-      executablePath:'./node_modules/chromium/lib/chromium/chrome-win/chrome.exe'
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
     })
 
  const page = await browser.newPage();
