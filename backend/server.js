3235a5cbb1744b3727c2812fca1d03122c485ea3
knight196 <71406824+knight196@users.noreply.github.com>
2023-03-12 14:00:45 +0000

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -91,7 +91,9 @@ app.post('/api/sendemail', async(req,res) => {
 
   
   try{
-    const browser =await puppeteer.launch();
+    const browser =await puppeteer.launch({
+      ignoreDefaultArgs:['--disable-extensions']
+    });
 
  const page = await browser.newPage();
 
