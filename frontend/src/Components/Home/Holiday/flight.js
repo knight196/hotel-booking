e80d25a864f6df36d4cb311c0b15d8d5ad71fddc
knight196 <bijaybaral55@gmail.com>
2024-06-14 12:35:17 +0100

    initial commit

---
+++ frontend/src/Components/Home/Holiday/flight.js
@@ -0,0 +1,26 @@
+const data = [
+{
+    image:'https://www.freightwaves.com/wp-content/uploads/2023/12/08/Cathay-Pacific-A350F_1.jpg',
+    title:'Cathay Pacific',
+    price: 300
+},
+{
+    image:'https://traveltradejournal.com/wp-content/uploads/2024/03/British-Airways.jpg',
+    title:'British Airways',
+    price: 300
+},
+{
+    image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTXJwF6otm2FDKE9F-dWTFX8NOE_ZZpNwYUr429jeF6WTgT4JSJor1p1NcLKIAZNoDxWY&usqp=CAU',
+    title:'Qatar Airways',
+    price: 350
+},
+{
+    image:'https://advanced-television.com/wp-content/uploads/2023/12/UnitedAirlines.png',
+    title:'United Airways',
+    price: 400
+},
+
+
+]
+
+export default data
